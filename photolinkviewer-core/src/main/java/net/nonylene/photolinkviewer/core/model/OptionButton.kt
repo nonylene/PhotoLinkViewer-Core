a70fbaa5fe9043a0571d891a02d1c237d40ac246
nonylene <cookie.speaker@gmail.com>
2016-08-24 02:27:34 +0900

    add text and temp add_button

--- photolinkviewer-core/src/main/java/net/nonylene/photolinkviewer/core/model/OptionButton.kt
+++ photolinkviewer-core/src/main/java/net/nonylene/photolinkviewer/core/model/OptionButton.kt
@@ -7,15 +7,16 @@ import net.nonylene.photolinkviewer.core.R
 
 private val BLUE_COLOR = Color.parseColor("#24a8ed")
 private val RED_COLOR = Color.parseColor("#f4554f")
+private val GREEN_COLOR = Color.parseColor("#46d249")
 
-enum class OptionButton(val id: Int, @DrawableRes val icon: Int, @ColorInt val color: Int) {
+enum class OptionButton(val id: Int, @DrawableRes val icon: Int, @ColorInt val color: Int, val text: String) {
 
-    TWEET_LIKE(0, R.drawable.plv_core_ic_favorite_white, RED_COLOR),
-    TWEET_RETWEET(1, R.drawable.plv_core_retweet_white, RED_COLOR),
-    DOWNLOAD(2, R.drawable.plv_core_ic_file_download_white, BLUE_COLOR),
-    PREFERENCE(3, R.drawable.plv_core_ic_settings_white, BLUE_COLOR),
-    OPEN_OTHER_APP(4, R.drawable.plv_core_ic_open_in_browser_white, BLUE_COLOR);
-
+    ADD_BUTTON(-1, R.drawable.plv_core_ic_expand_less_white, GREEN_COLOR, "Add Button"),
+    TWEET_LIKE(0, R.drawable.plv_core_ic_favorite_white, RED_COLOR, "Like tweet"),
+    TWEET_RETWEET(1, R.drawable.plv_core_retweet_white, RED_COLOR, "Retweet"),
+    DOWNLOAD(2, R.drawable.plv_core_ic_file_download_white, BLUE_COLOR, "Download"),
+    PREFERENCE(3, R.drawable.plv_core_ic_settings_white, BLUE_COLOR, "Preference"),
+    OPEN_OTHER_APP(4, R.drawable.plv_core_ic_open_in_browser_white, BLUE_COLOR, "Open browser");
 }
 
 fun getOptionButtonFromId(id: Int): OptionButton {
