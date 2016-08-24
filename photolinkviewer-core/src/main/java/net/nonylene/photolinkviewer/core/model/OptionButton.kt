56269e42318177fffffdfa01c4062a68d30f2560
nonylene <cookie.speaker@gmail.com>
2016-08-25 00:44:57 +0900

    add button to string null

--- photolinkviewer-core/src/main/java/net/nonylene/photolinkviewer/core/model/OptionButton.kt
+++ photolinkviewer-core/src/main/java/net/nonylene/photolinkviewer/core/model/OptionButton.kt
@@ -9,9 +9,9 @@ private val BLUE_COLOR = Color.parseColor("#24a8ed")
 private val RED_COLOR = Color.parseColor("#f4554f")
 private val GREEN_COLOR = Color.parseColor("#46d249")
 
-enum class OptionButton(val id: Int, @DrawableRes val icon: Int, @ColorInt val color: Int, val text: String) {
+enum class OptionButton(val id: Int, @DrawableRes val icon: Int, @ColorInt val color: Int, val text: String?) {
 
-    ADD_BUTTON(-1, R.drawable.plv_core_ic_expand_less_white, GREEN_COLOR, "Add Button"),
+    ADD_BUTTON(-1, R.drawable.plv_core_ic_add_24dp, GREEN_COLOR, null),
     TWEET_LIKE(0, R.drawable.plv_core_ic_favorite_white, RED_COLOR, "Like tweet"),
     TWEET_RETWEET(1, R.drawable.plv_core_retweet_white, RED_COLOR, "Retweet"),
     DOWNLOAD(2, R.drawable.plv_core_ic_file_download_white, BLUE_COLOR, "Download"),
